ce171bccb62ddb0e3aca5cbbfb501df0e1cde31b
Aleksandr Kamlykov <kundili093@gmail.com>
2021-12-16 13:47:24 +0200

    fix

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -19,6 +19,7 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
+
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
@@ -31,7 +32,6 @@ export function register(config) {
 
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
@@ -98,7 +98,6 @@ function registerValidSW(swUrl, config) {
 
 function checkValidServiceWorker(swUrl, config) {
   // Check if the service worker can be found. If it can't reload the page.
-  console.log('tut')
   fetch(swUrl, {
     headers: { 'Service-Worker': 'script' },
   })
